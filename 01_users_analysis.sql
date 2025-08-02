54515d0f1da6a13ae50d1c00500720408354c07a
Erez Newman <ereznewman@gmail.com>
2025-08-02 09:33:55 +0100

    Create 01_users_analysis.sql

---
+++ 01_users_analysis.sql
@@ -0,0 +1,11 @@
+--Who are the top 5 users by number of sessions?
+
+
+
+--How many distinct users visited the site in total?
+
+--Which users have the highest number of purchases (events)?
+
+--What is the distribution of users by country?
+
+--Which user has the longest average session duration?
