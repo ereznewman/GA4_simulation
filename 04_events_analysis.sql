f0975b46f679340725912359cfc92b00d3af7a26
Erez Newman <ereznewman@gmail.com>
2025-08-02 10:10:26 +0100

    Update 04_events_analysis.sql

--- 04_events_analysis.sql
+++ 04_events_analysis.sql
@@ -1,1 +1,9 @@
+--What are the most frequent event types overall?
 
+--Which users triggered the highest number of purchase events?
+
+--How does event frequency vary by hour of day?
+
+--What percentage of sessions included an add_to_cart event?
+
+--How many events are recorded per product?
