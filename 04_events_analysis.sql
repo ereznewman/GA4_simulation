a7abbf6050ac771710c84b6b8bd7258cbda8a625
Erez Newman <ereznewman@gmail.com>
2025-08-02 10:59:29 +0100

    Update 04_events_analysis.sql

--- 04_events_analysis.sql
+++ 04_events_analysis.sql
@@ -1,5 +1,9 @@
 --What are the most frequent event types overall?
 
+SELECT event_type,COUNT(*) AS count FROM events
+GROUP BY event_type
+ORDER BY count DESC
+
 --Which users triggered the highest number of purchase events?
 
 --How does event frequency vary by hour of day?
