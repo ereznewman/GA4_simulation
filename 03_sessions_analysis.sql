38c88e3583575ad31b1907b970d5a294a0d28559
Erez Newman <ereznewman@gmail.com>
2025-08-02 10:10:07 +0100

    Update 03_sessions_analysis.sql

--- 03_sessions_analysis.sql
+++ 03_sessions_analysis.sql
@@ -1,1 +1,9 @@
+--What are the top traffic sources driving sessions?
 
+--What is the average session duration across all sessions?
+
+--How many sessions started from organic search vs direct vs referral?
+
+--Which day of the week has the highest session count?
+
+--What is the average number of sessions per user?
