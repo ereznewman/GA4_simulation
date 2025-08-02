50aa3b33d5ab01967528e64d822c84c1934de08e
Erez Newman <ereznewman@gmail.com>
2025-08-02 11:04:46 +0100

    Update 04_events_analysis.sql

--- 04_events_analysis.sql
+++ 04_events_analysis.sql
@@ -4,10 +4,4 @@ SELECT event_type,COUNT(*) AS count FROM events
 GROUP BY event_type
 ORDER BY count DESC
 
---Which users triggered the highest number of purchase events?
 
---How does event frequency vary by hour of day?
-
---What percentage of sessions included an add_to_cart event?
-
---How many events are recorded per product?
