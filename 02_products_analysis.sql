b4a73f8991f0d2c0716575415fb9d52ccc85f3e0
Erez Newman <ereznewman@gmail.com>
2025-08-02 10:09:48 +0100

    Update 02_products_analysis.sql

--- 02_products_analysis.sql
+++ 02_products_analysis.sql
@@ -1,1 +1,9 @@
+--What are the top 5 products viewed most often?
 
+--Which products have the highest number of add_to_cart events?
+
+--How many unique users interacted with each product?
+
+--What is the average price of products by category?
+
+--Which products resulted in the highest purchase events?
